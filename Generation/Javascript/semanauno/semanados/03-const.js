d6a3977731c7333b36e24cb043a21073be6a1f8f
Tavusia <tavo.0540@gmail.com>
2021-12-09 11:39:35 -0600

    Nuevos ejercicios sobre el tipo de variables: globales, locales y constantes.

---
+++ Generation/Javascript/semanauno/semanados/03-const.js
@@ -0,0 +1,11 @@
+/* 
+const se utiliza para declarar constantes, tiene alcance de contexto
+vive en un bloque de codigo.
+*/
+
+const gravedad = 9.8;
+/*gravedad = 30; marca error debido a esto pues la variable constante 
+solo puede asignarsele un valor una vez, después ya no.
+*/
+
+console.log(gravedad);
