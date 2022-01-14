48a3a1516260f78b0a7e7b493f0e6fa410ddb541
Octavio Olea <oleaoctavio23@gmail.com>
2022-01-13 18:25:40 -0600

    Solucionado ejercicio práctico de mostrar la fecha después de un año de un input date.

--- javascript-avanzado/assets/js/date-practica.js
+++ javascript-avanzado/assets/js/date-practica.js
@@ -1,8 +1,22 @@
+/* Dada una fecha insertada por el usuario que muestra en otro input al seleccionarla la misma fecha
+pero dentro de un año */
+/* Por ejemplo si hoy es 13 de enero del 2022 que muestre en el otro input
+13 de enero del 2023 */
 let date1 = document.getElementById('date1');
 let date2 = document.getElementById('date2');
 
-date1.addEventListener("change",()=>{
-    let fechaObtenida = new Date(date1.value);
-    let anioInicial = fechaObtenida.getFullYear();
-    
-})
+date1.addEventListener("change",(e)=>{
+
+    let fechaInicial = e.target.value;
+    console.log(`Fecha inicial: ${fechaInicial}`);
+    let anioInicial = parseInt(e.target.value);
+    let anioFinal=++anioInicial;
+    let anioFinalString=anioFinal.toString();
+    let subStringFechaFinal=fechaInicial.substr(4,9);
+    let fechaFinalString = anioFinalString.concat(subStringFechaFinal);
+    console.log(subStringFechaFinal);
+    console.log(typeof(anioFinalString));
+    console.log(`Año final: ${anioFinal}`);
+    date2.value = fechaFinalString;
+    console.log(date2.value);
+});
