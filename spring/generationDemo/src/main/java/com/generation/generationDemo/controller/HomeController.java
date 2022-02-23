a53633ded235e272ed20431b51526aae5a5b0e13
Octavio Olea <oleaoctavio23@gmail.com>
2022-02-23 12:55:51 -0600

    pruebas con postman de tipo get y post

--- spring/generationDemo/src/main/java/com/generation/generationDemo/controller/HomeController.java
+++ spring/generationDemo/src/main/java/com/generation/generationDemo/controller/HomeController.java
@@ -1,10 +1,10 @@
 package com.generation.generationDemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/index")
@@ -13,9 +13,15 @@ public class HomeController {
 	public String saludo() {
 		return "HOLA";
 	}
-	
+
 	@GetMapping("/saludo")
-	public String saludar(@RequestParam(value="nombre", defaultValue = "java")String nombre) {
-		return "Hola " + nombre;
+	public String saludar(@RequestParam(value = "nombre", defaultValue = "java") String nombre
+			,@RequestParam(value="apellido") String apellido) {
+		return nombre + apellido;
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam(value="email")String correo, @RequestParam(value="password")String contrasenia) {
+		return correo + "-" + contrasenia;
 	}
 }
