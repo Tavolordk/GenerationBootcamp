a66c6bd9df4e878f3e4b431450bb326462fe6f00
Octavio Olea <oleaoctavio23@gmail.com>
2022-02-28 19:37:10 -0600

    Uso de CRUDREPOSITORY y de el RequestMapping.

---
+++ spring/ProyectoSpring/src/main/java/com/generation/restcontroller/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.generation.restcontroller.controller;
+
+import com.generation.restcontroller.model.User;
+import com.generation.restcontroller.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(@Autowired UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+
+    @GetMapping("/all")
+    public ArrayList<User> getUsers() {
+        return (ArrayList<User>) userService.getUsers();
+    }
+    
+    @GetMapping("/search")
+    public ArrayList<User> findByName(@RequestParam(name = "name") String name){
+    	return (ArrayList<User>) userService.findByName(name);
+    }
+}
